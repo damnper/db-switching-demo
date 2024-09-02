400872e67ea9a51fa5257fa1296db82a6716e00b
Daler Yunusov <daleryunusov2001@gmail.com>
2024-09-02 13:20:21 +0300

    Initial commit

---
+++ src/main/java/com/example/dbswitchingdemo/DbSwitchingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbswitchingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbSwitchingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbSwitchingDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dbswitchingdemo/DbSwitchingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbswitchingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbSwitchingDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
